e85ca3598b7f32f53718afff4b82b87c14c57a6c
1chard <1chard@github.com>
2021-10-22 16:59:16 -0300

    cores

--- modules/tema.js
+++ modules/tema.js
@@ -5,10 +5,10 @@ const temaClaro = () => {
     document.body.style.setProperty("--main-extra", "#e09a30");
     document.body.style.setProperty("--background-color", "#fff");
     document.body.style.setProperty("--background-extra", "#faf8ef");
-    document.body.style.setProperty("--secondary-color", "#951");
-    document.body.style.setProperty("--secondary-extra", "#a72");
-    document.body.style.setProperty("--text-color", "#111");
-    document.body.style.setProperty("--text-extra", "#302005");
+    document.body.style.setProperty("--secondary-color", "#b97");
+    document.body.style.setProperty("--secondary-extra", "#eca");
+    document.body.style.setProperty("--text-color", "#030303");
+    document.body.style.setProperty("--text-extra", "#432");
 }
 
 const temaEscuro = () => {
@@ -17,8 +17,12 @@ const temaEscuro = () => {
     document.body.style.setProperty("--main-extra", "#24272b");
     document.body.style.setProperty("--background-color", "#1e2226");
     document.body.style.setProperty("--background-extra", "#161b20");
-    document.body.style.setProperty("--secondary-color", "#ea1");
-    document.body.style.setProperty("--secondary-extra", "#61a");
+    document.body.style.setProperty("--secondary-color", "#e58");
+    document.body.style.setProperty("--secondary-extra", "#c47");
+    /*
+     * document.body.style.setProperty("--secondary-color", "#e8a");
+    document.body.style.setProperty("--secondary-extra", "#c78");
+     */
     document.body.style.setProperty("--text-color", "#eec");
     document.body.style.setProperty("--text-extra", "#ed9");
 }
